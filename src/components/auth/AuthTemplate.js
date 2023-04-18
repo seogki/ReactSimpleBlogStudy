33a12e93dc33b63aa17f6c201a6f29ccfee04431
skh <seogkihong333@gmail.com>
2023-04-18 23:49:46 +0900

    redux / styled-component 구축

---
+++ src/components/auth/AuthTemplate.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+const AuthTemplateBlock = styled.div``;
+const AuthTemplate = ({ children }) => {
+  return <AuthTemplateBlock>{children}</AuthTemplateBlock>;
+};
+
+export default AuthTemplate;
